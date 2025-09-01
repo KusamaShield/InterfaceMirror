403f581a315cbda614bbc066c76981ebcad4c82b
0xCC43 <hi@localhost>
2025-09-01 16:15:52 +0700

    paseo + kusama

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { comlink } from "vite-plugin-comlink";
+//import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
